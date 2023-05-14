01f9ba37162e3de80f8273ea12ee20d844d10ec5
kofikg <kofiivan86@gmail.comgit config --global user.email kofiivan86@gmail.com>
2023-05-13 19:53:04 -0700

    Updated

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is the module class for review"""
+from models.base_model import BaseModel
+
+""" This is the public class attribute for Review"""
+place_id = " "
+user_id = " "
+text = " "
+
